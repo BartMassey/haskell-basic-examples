eaf21ade24d0dbaca6cd0207417cf0f51245dccf
Bart Massey <bart@cs.pdx.edu>
2016-06-10 07:04:52 -0700

    cleaned up rna2 by using Data.List.find

--- rna2.hs
+++ rna2.hs
@@ -3,6 +3,8 @@
 -- Please see the file COPYING in the source
 -- distribution of this software for license terms.
 
+import Data.List (find)
+
 -- | Group a list into chunks of specified size, with
 -- any shortage in the last chunk.
 chunk :: Int -> [a] -> [[a]]
@@ -11,19 +13,27 @@ chunk n ws =
     let (start, rest) = splitAt n ws in
     start : chunk n rest
 
--- | Return the first match of the RNA triple in
--- the AA decoding table.
-findTriple :: [[String]] -> String -> String
-findTriple [] triple = error ("triple not found: " ++ triple)
-findTriple ([pattern, target] : _) triple 
-    | and (zipWith sloppyEqual pattern triple) = target
+-- | Return true iff the target matches the AA decoding table pattern.
+okTriple :: String -> [String] -> Bool
+okTriple triple [pattern, _] | length triple /= length pattern = False
+okTriple triple [pattern, _] =
+    and (zipWith sloppyEqual pattern triple)
     where
-      sloppyEqual '_' target = True
-      sloppyEqual pattern target = pattern == target
-findTriple (_ : table) triple =
-    findTriple table triple
+      sloppyEqual '_' _ = True
+      sloppyEqual patternChar tripleChar = patternChar == tripleChar
+okTriple _ entry = error ("bad decoding table entry: " ++ show entry)
 
--- | Read in the RNA -> AA table and process the input.
+-- | Look up the appropriate pattern in the decoding table
+-- and return the matching target.
+findTriple :: [[String]] -> String -> String
+findTriple table triple =
+    case find (okTriple triple) table of
+      Just [_, target] -> target
+      _ -> error ("triple not found: " ++ triple)
+
+-- | Read in the RNA -> AA table and process the input. Note that the use
+-- of "triple" in this code is misleading: it will work fine for any
+-- nonzero-length table patterns and input strings.
 main :: IO ()
 main = do
   tableData <- readFile "rna.txt"
